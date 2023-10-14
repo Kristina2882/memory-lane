c61e922c447882c451060bd4a8c6875717636d81
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2023-10-14 18:59:16 +0300

    Reusableorm added.

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,12 +1,45 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function ReusableForm(props) {
     return (
 <React.Fragment>
-    
+<form onSubmit={props.handleFormSubmission} >
+        <input
+        type="text"
+        name="name"
+        placeholder="Your name"
+        />
+         <input
+        type="text"
+        name="memoText"
+        placeholder="Your dream"
+        />
+         <input
+        type="text"
+        name="date"
+        placeholder="Date"
+        />
+         <input
+        type="text"
+        name="rate"
+        placeholder="Stars"
+        />
+         <input
+        type="text"
+        name="emotion"
+        placeholder="Your emotion"
+        />
+        <button type="submit">{props.btnText}</button>
+    </form>
 </React.Fragment>
     );
 }
- 
 
+ReusableForm.propTypes = {
+    
+    handleFormSubmission: PropTypes.func,
+    btnText: PropTypes.string
+};
+ 
 export default ReusableForm;
